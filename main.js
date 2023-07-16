f6540c4772a58d7ef1e96978ea1687988bccd3a4
Yugesh Pandey <27809103+yugeshpandey@users.noreply.github.com>
2023-07-15 21:34:34 -0600

    objLoader added

--- main.js
+++ main.js
@@ -1,26 +1,50 @@
-import * as THREE from 'three';
+import * as THREE from 'three'
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js'
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+let scene, camera, renderer, object;
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+async function init() {
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+    renderer = new THREE.WebGLRenderer();
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
 
-camera.position.z = 5;
+	 // Lighting
+	 const ambientLight = new THREE.AmbientLight(0x404040, 2); // Soft white light
+	 scene.add(ambientLight);
+ 
+	 const pointLight = new THREE.PointLight(0xffffff, 1, 500); // White light
+	 pointLight.position.set(10, 10, 10);
+	 scene.add(pointLight);
 
-function animate() {
-	requestAnimationFrame( animate );
+    // Load the OBJ file
+    const loader = new OBJLoader();
+    object = await loader.loadAsync('/assets/Brain_Model.obj');
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+    object.rotation.y = 0;
+	object.visible = true;
+	
+    scene.add(object);
 
-	renderer.render( scene, camera );
+    camera.position.z = 5;
 }
 
+function animate() {
+    requestAnimationFrame(animate);
+    if (object) {
+        object.rotation.y += 0.01; // Rotates the object at each frame
+    }
+    renderer.render(scene, camera);
+}
+
+// Resize canvas on window resize
+//window.addEventListener('resize', function(){
+//    camera.aspect = window.innerWidth / window.innerHeight;
+//    camera.updateProjectionMatrix();
+//    renderer.setSize(window.innerWidth, window.innerHeight);
+//}, false);
+
+init();
 animate();
